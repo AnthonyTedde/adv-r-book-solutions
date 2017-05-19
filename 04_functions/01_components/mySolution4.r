d9991e9c843262e52184530b770e599d565c2e73
Anthony Tedde <antho.tedde@gmail.com>
2017-05-19 11:34:14 +0200

    Function > mySolution4

---
+++ 04_functions/01_components/mySolution4.r
@@ -0,0 +1,8 @@
+# 4. When does printing a function not show what environment it was created in?
+print(matrix)
+print(c)
+# With primitive function ? the environment is not shown with print(c) and c is a primitive function
+
+
+# Correction:
+# For primitive function AND for function created in GlobalEnv
